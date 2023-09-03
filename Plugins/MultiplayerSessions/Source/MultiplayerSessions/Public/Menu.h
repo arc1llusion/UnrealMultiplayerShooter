258e5604dca1c93c7df90aeb3d3f923de3b128d8
Arc1llusion <arc1llusion@gmail.com>
2023-09-03 10:45:34 -0400

    Up to lecture 241 - Select Match Type

--- Plugins/MultiplayerSessions/Source/MultiplayerSessions/Public/Menu.h
+++ Plugins/MultiplayerSessions/Source/MultiplayerSessions/Public/Menu.h
@@ -60,6 +60,8 @@ private:
 	class UMultiplayerSessionsSubsystem* MultiplayerSessionsSubsystem;
 
 	int32 NumPublicConnections{4};
+
+	UPROPERTY(BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
 	FString MatchType{TEXT("FreeForAll")};
 
 	FString PathToLobby{TEXT("")};
